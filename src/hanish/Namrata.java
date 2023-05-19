e3eee845fc088231d62bb08d75084272e877c2ea
konda30 <124072505+konda30@users.noreply.github.com>
2023-05-19 13:51:09 -0700

    SecondCommit

---
+++ src/hanish/Namrata.java
@@ -0,0 +1,21 @@
+package hanish;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Namrata
+{
+		public static void main(String[] args) {
+					   
+		   System.setProperty("webdriver.chrome.driver","C://Users//Maharshi//Desktop//SeleniumJars//chromedriver_win32//chromedriver.exe");
+		   WebDriver driver = new ChromeDriver();
+		   driver.get("http://www.facebook.com/");
+	         driver.manage().window().maximize();
+	         //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));						}
+}
+}
+
+
